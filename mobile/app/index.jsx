f6e2a0e45f56efb4e3eb7ae95d90b690e18d69bc
pawan1-tech <pawansah282@gmail.com>
2025-04-29 11:38:40 +0530

    Mobile app setup

---
+++ mobile/app/index.jsx
@@ -0,0 +1,23 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style = {styles.title} >Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "blue",
+  }
+});
