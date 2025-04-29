63a549f511384f5f8412ff046df7a843e0480865
pawan1-tech <pawansah282@gmail.com>
2025-04-29 13:19:03 +0530

    Auth Route setup

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,10 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Image } from "expo-image";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style = {styles.title} >Hello World</Text>
+      <Link href={"/(auth)/signup"} >SignUp</Link>
+      <Link href={"/(auth)"} >Login</Link>
     </View>
   );
 }
